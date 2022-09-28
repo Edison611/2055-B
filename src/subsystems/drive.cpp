4d95bb7874d9d3cc4176a78095f670cc94e049d2
Edison611 <edisony611@gmail.com>
2022-09-28 09:01:13 -0400

    Bug Fix

--- src/subsystems/drive.cpp
+++ src/subsystems/drive.cpp
@@ -4,6 +4,7 @@
 #include "pros/misc.h"
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 
 // DRIVER CONTROL FUNCTIONS
@@ -13,66 +14,55 @@ void setDrive(int left, int right) {
     driveLeftFront.move(left);
     driveRightBack.move(right);
     driveRightFront.move(right);
+    pros::lcd::set_text(3, std::to_string(left));
+    pros::lcd::set_text(4, std::to_string(right));
 }
 
 
 void setDriveMotors() {
-    /*
-    double turn = 0;
-    double drive = 0;
-
-    int y = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-    int x = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-
-    if (abs(x) < 10) 
-        x = 0;
-    if (abs(y) < 10) 
-        y = 0;
-    setDrive(x, y);
-    */
     
     int deadband = 5;
-    int drive = 0;
-    int turn = 0;
 
-    int x = abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X));
-    int y = abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+    double drive = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    double turn = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+
+    pros::lcd::set_text(5, "drive: " + std::to_string(drive));
+    pros::lcd::set_text(6, "turn: " + std::to_string(turn));
 
     // Robot will not move with slight joystick movement
     /*
-    if (abs(y) > deadband || abs(x) > deadband){
+    if (abs(drive) > deadband || abs(turn) > deadband){
     // Makes the bot move exponentially fast
-        if (abs(y) > 50){
-            drive = (10/(1+exp(-(abs(y)/10) - 5))) * 1.2 - 0.5;
+        if (abs(drive) > 50){
+            drive = (10/(1+exp(-(abs(drive)/10) - 5))) * 1.2 - 0.5;
         }
         else {
-            drive = (5*pow((1/5)*(abs(y-5))/10, 3.0)) * 1.2 ;
+            drive = (5*pow((1/5)*(abs(drive-5))/10, 3.0)) * 1.2 ;
         }
-        if (abs(x) > 50){
-            drive = (10/(1+exp(-(abs(x)/10) - 5))) * 1.2 - 0.5;
+        if (abs(turn) > 50){
+            drive = (10/(1+exp(-(abs(turn)/10) - 5))) * 1.2 - 0.5;
         }
         else {
-            drive = (5*pow((1/5)*(abs(x-5))/10, 3.0)) * 1.2 ;
+            drive = (5*pow((1/5)*(abs(turn-5))/10, 3.0)) * 1.2 ;
         }
     }
-
+  
     else {
         drive = 0;
         turn = 0;
     }
-    */
+    
     // Reverses controls for the other direction
-    if (y < 0){
+    if (drive < 0){
         drive = -drive;
     }
-    if (x < 0){
+    if (turn < 0){
         turn = -turn;
     }
-    double left = drive - turn;
-    double right = drive + turn;
+    */
+    double left = drive + turn;
+    double right = drive - turn;
 
     setDrive(left, right);
 
 }
-
-
