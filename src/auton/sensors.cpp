c04a378a541dda17c9e67f613053e2457faf1830
Edison611 <edisony611@gmail.com>
2022-10-07 18:55:50 -0400

    Added Basic Encoder Functions

--- src/auton/sensors.cpp
+++ src/auton/sensors.cpp
@@ -1,4 +1,12 @@
+#include "main.h"
 
 void reset_sensors() {
-    
+    encLeft.reset();
+    encRight.reset();
+    encMiddle.reset();
+}
+
+double averagePos() {
+    double average = (encRight.get_value() + encLeft.get_value() + encMiddle.get_value())/3.0;
+    return average;
 }
